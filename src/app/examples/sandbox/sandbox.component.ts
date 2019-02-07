556b76796d84542b9892358d72daab3d2a7ea0ee
Chris Trześniewski <krzysztof.trzesniewski@scalac.io>
2019-02-07 12:22:33 +0100

    2019-01-30 Angular Wroclaw

--- src/app/examples/sandbox/sandbox.component.ts
+++ src/app/examples/sandbox/sandbox.component.ts
@@ -1,19 +1,22 @@
 import {Component} from '@angular/core';
-import {merge, Observable, OperatorFunction, Subject} from 'rxjs';
+import {merge, Observable, of, OperatorFunction, Subject, zip} from 'rxjs';
 import {CoffeeRequest, CoffeeRequestStatusValue, createCoffeeRequest, idGenerator, setStatus} from '../../coffee-request';
-import {delay, map, scan, share, startWith} from 'rxjs/operators';
+import {delay, map, mapTo, scan, share, startWith, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-sandbox',
   template: `
     <h1>Sandbox</h1>
     <button mat-raised-button (click)="clicks$.next()">Add order</button>
+    <button mat-raised-button (click)="baristaCheckIn.next()">Barista available</button>
+    <div>Baristas available: {{baristasAvaiable$ | async}}</div>
     <app-coffee-items [items]="state$ | async"></app-coffee-items>
   `,
   styleUrls: ['./sandbox.component.scss']
 })
 export class SandboxComponent {
   clicks$: Subject<void> = new Subject();
+  baristaCheckIn = new Subject();
   coffeeReqs$: Observable<CoffeeRequest> = this.clicks$.pipe(
     map(idGenerator()),
     map(createCoffeeRequest),
@@ -21,6 +24,9 @@ export class SandboxComponent {
   );
   coffeeMaking$: Observable<CoffeeRequest> = this.coffeeReqs$.pipe(this.assignBarista());
   coffeeDone$: Observable<CoffeeRequest> = this.coffeeMaking$.pipe(this.makeCoffee());
+  barista$ = of(null).pipe(switchMap(() => {
+    return merge(this.baristaCheckIn.asObservable(), this.coffeeDone$);
+  }));
   coffeePickedUp$: Observable<CoffeeRequest> = this.coffeeDone$.pipe(this.pickupCoffee());
   state$: Observable<CoffeeRequest[]> =
     merge(this.coffeeReqs$, this.coffeeMaking$, this.coffeeDone$, this.coffeePickedUp$).pipe(
@@ -37,9 +43,17 @@ export class SandboxComponent {
       startWith([])
     );
 
+  baristasAvaiable$ = merge(
+    this.barista$.pipe(mapTo(1)),
+    this.coffeeMaking$.pipe(mapTo(-1))
+  ).pipe(
+    scan((baristas, change) => baristas + change, 0),
+    startWith(0),
+  );
+
   assignBarista(): OperatorFunction<CoffeeRequest, CoffeeRequest> {
-    return (source: Observable<CoffeeRequest>) => source.pipe(
-      delay(1000),
+    return (source: Observable<CoffeeRequest>) => zip(source, this.barista$).pipe(
+      map(([s]) => s),
       setStatus(CoffeeRequestStatusValue.making)
     );
   }
