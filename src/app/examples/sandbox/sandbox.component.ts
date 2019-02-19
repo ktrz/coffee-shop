5942f9ab95d65d7babe80ccfc7c92266e83f4e1b
Chris Trześniewski <krzysztof.trzesniewski@scalac.io>
2019-02-19 22:18:33 +0100

    Angular Tricity - after live coding

--- src/app/examples/sandbox/sandbox.component.ts
+++ src/app/examples/sandbox/sandbox.component.ts
@@ -1,19 +1,26 @@
 import {Component} from '@angular/core';
-import {merge, Observable, OperatorFunction, Subject} from 'rxjs';
+import {merge, Observable, of, OperatorFunction, Subject, zip} from 'rxjs';
 import {CoffeeRequest, CoffeeRequestStatusValue, createCoffeeRequest, idGenerator, setStatus} from '../../coffee-request';
-import {delay, map, scan, share, startWith} from 'rxjs/operators';
+import {delay, map, mapTo, scan, share, startWith, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-sandbox',
   template: `
     <h1>Sandbox</h1>
     <button mat-raised-button (click)="clicks$.next()">Add order</button>
+    <button mat-raised-button (click)="baristaCheckIn$.next()">Barista check in</button>
+    <div>Baristas available: {{baristasAvailable$ | async}}</div>
     <app-coffee-items [items]="state$ | async"></app-coffee-items>
   `,
   styleUrls: ['./sandbox.component.scss']
 })
 export class SandboxComponent {
   clicks$: Subject<void> = new Subject();
+  baristaCheckIn$: Subject<void> = new Subject();
+  barista$ = of(null).pipe(switchMap(() => merge(this.baristaCheckIn$, this.coffeeDone$)));
+  // After presentation I was suggested with the following solution instead of using `of` and `switchMap`
+  // `defer` can be imported from 'rxjs'
+  // barista$ = defer(() => merge(this.baristaCheckIn$, this.coffeeDone$));
   coffeeReqs$: Observable<CoffeeRequest> = this.clicks$.pipe(
     map(idGenerator()),
     map(createCoffeeRequest),
@@ -37,9 +44,17 @@ export class SandboxComponent {
       startWith([])
     );
 
+  baristasAvailable$ = merge(
+    this.barista$.pipe(mapTo(1)),
+    this.coffeeMaking$.pipe(mapTo(-1))
+  ).pipe(
+    scan((baristas, change) => baristas + change, 0),
+    startWith(0),
+  );
+
   assignBarista(): OperatorFunction<CoffeeRequest, CoffeeRequest> {
-    return (source: Observable<CoffeeRequest>) => source.pipe(
-      delay(1000),
+    return (source: Observable<CoffeeRequest>) => zip(source, this.barista$).pipe(
+      map(([s]) => s),
       setStatus(CoffeeRequestStatusValue.making)
     );
   }
